76a68c74dbd30239976ab70656af9e9f816a753d
Your Name <minhkhoimk88@gmail.com>
2019-07-23 16:49:20 +0700

    sign in up mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :posts
   has_many :reports
   has_many :comments
@@ -15,6 +16,34 @@ class User < ApplicationRecord
   has_many :notifications, foreign_key: :receiver, dependent: :destroy
   has_many :reverse_notifications, foreign_key: :creator, dependent: :destroy,
               class_name: Notification.name
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.email_max},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: {maximum: Settings.name_max}
+  validates :password, presence: true, length: {minimum: Settings.pass_min},
+                    allow_nil: true
+
+  before_create :create_activation_token
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def activate
+    update_columns activated: true
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_activation_token
+    self.activation_token = User.new_token
+  end
 
   def feed
     Post.post_auth id
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
     resources :reactions, only: [:create, :destroy, :update]
     resources :comments, only: [:create, :destroy, :show, :edit, :update]
   end
+  root "users#index"
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  post "login", to: "sessions#create", as: "sessions"
+  get "logout", to: "sessions#destroy", as: "logout"
+  resources :users
+  resources :account_activations, only: :edit
 end
